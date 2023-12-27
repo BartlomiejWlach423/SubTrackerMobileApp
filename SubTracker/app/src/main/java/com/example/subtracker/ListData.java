ec5c67b10ccda41ffd8dffa9bba1cc1de975c447
BartlomiejWlach423 <bartekwlach423@gmail.com>
2023-12-27 14:33:34 +0100

    ArrayAdapter
    unfinished ArrayAdapter implementation to store objects

---
+++ SubTracker/app/src/main/java/com/example/subtracker/ListData.java
@@ -0,0 +1,14 @@
+package com.example.subtracker;
+
+public class ListData {
+    String name;
+    Float price;
+    int DayOfMonth, image;
+
+    public ListData(String name, Float price, int dayOfMonth, int image) {
+        this.name = name;
+        this.price = price;
+        DayOfMonth = dayOfMonth;
+        this.image = image;
+    }
+}
--- SubTracker/app/src/main/java/com/example/subtracker/MainActivity.java
+++ SubTracker/app/src/main/java/com/example/subtracker/MainActivity.java
@@ -9,7 +9,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> subList = new ArrayList<String>();
     ArrayList<Float> priceList = new ArrayList<Float>();
+    ArrayList<Integer> dateList = new ArrayList<Integer>();
 
 
     @Override
@@ -27,17 +31,23 @@ public class MainActivity extends AppCompatActivity {
         subList.add("obj2");
         priceList.add(12.0f);
         priceList.add(34.0f);
+        dateList.add(12);
+        dateList.add(14);
     }
 
     public void onClickAdd(View view) {
         EditText editTextName = (EditText) findViewById(R.id.editTextName);//pobranie wartości editText z nazwą abonamentu
         EditText editTextPrice = (EditText) findViewById(R.id.editTextPrice);//pobranie wartości editText z ceną abonamentu
+        EditText editTextDate = (EditText) findViewById(R.id.editTextDate);//pobranie wartości editText z datą pobierania płatności abonamentu
+        dateList.add(Integer.valueOf(editTextDate.getText().toString()));
         subList.add(editTextName.getText().toString());//dodanie pobranych danych do listy z nazwami
         priceList.add(Float.valueOf(editTextPrice.getText().toString()));//dodanie pobranych danych do listy z cenami
         TextView txtViewName = (TextView) findViewById(R.id.textViewName);//inicjalizacja textView z abonamentami
         TextView txtViewPrice = (TextView) findViewById(R.id.textViewPrice);//inicjalizacja textView z cenami
+        TextView txtViewDate = (TextView) findViewById(R.id.textViewDate);
         String prompt = "Abonamenty:\n";//inicjalizacja wartości aktualizującej komunikat o zawartości subList
         String prompt2 = "Ceny:\n";//inicjalizacja wartości aktualizującej komunikat o zawartości priceList
+        String prompt3 = "Dzień miesiąca:\n";
         //dodawanie elementów do textView
         for (String element : subList) {
             prompt+=element.toString()+"\n";
@@ -45,10 +55,15 @@ public class MainActivity extends AppCompatActivity {
         for (Float element : priceList) {
             prompt2+=element.toString()+"\n";
         }
+        for (Integer element : dateList) {
+            prompt3+=element.toString()+" every month\n";
+        }
         txtViewName.setText(prompt);//wyświetlenie listy z abonamentami
         txtViewPrice.setText(prompt2);//wyświetlenie listy z cenami
+        txtViewDate.setText(prompt3);
         editTextName.setText("");//wyczyszczenie editTextName
         editTextPrice.setText("");//wyczyszczenie editTextPrice
+        editTextDate.setText("");
     }
 
     public void OnClickBackToMain(View view) {
